f993cfb08ea5e0b8a48645955b33986a7fe8bd1a
andymedina1 <107598069+andymedina1@users.noreply.github.com>
2024-01-19 12:03:36 -0300

    Create playRound function

--- script.js
+++ script.js
@@ -10,3 +10,47 @@ function getComputerChoice() {
   }
 }
 
+function getPlayerSelection() {
+  return prompt('Rock Paper or Scissors?')
+}
+
+function playRound(playerSelection, computerSelection) {
+  const player = playerSelection.toLowerCase()
+  const computer = computerSelection.toLowerCase()
+  if (player === computer) {
+    console.log("It's a tie!")
+    alert("It's a tie!")
+    return playRound(getPlayerSelection(), getComputerChoice())
+  }
+
+  if (player === 'paper') {
+    if (computer === 'rock') {
+      return "You Win! Paper beats Rock"
+    }
+    if (computer === 'scissors') {
+      return "You Lose! Scissors beats Paper"
+    }
+  }
+
+  if (player === 'rock') {
+    if (computer === 'paper') {
+      return "You Lose! Paper beats Rock"
+    }
+    if (computer === 'scissors') {
+      return "You Win! Rock beats Scissors"
+    }
+  }
+
+  if (player === 'scissors') {
+    if (computer === 'rock') {
+      return "You Lose! Rock beats Scissors"
+    }
+    if (computer === 'paper') {
+      return "You Win! Scissors beats Paper"
+    }
+  }
+}
+
+const playerSelection = getPlayerSelection();
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
